843caed50445dd08b4ba96a0d2290c3b35d7cf77
Tiago-0liveira <tfgoliveira@gmail.com>
2021-04-05 23:35:58 +0100

    basic types for api endpoints and more

---
+++ @types/interfaces/user.ts
@@ -0,0 +1,7 @@
+
+export default interface IUser {
+    username: string
+    displayName: string | null
+    photoUrl: string | null
+    uid: string
+}
---
+++ @types/types/users.ts
@@ -0,0 +1,5 @@
+import { User } from "@prisma/client"
+
+type TUsers = User[]
+
+export default TUsers
